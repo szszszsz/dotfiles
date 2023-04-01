8911c40e50cd9ebcfc72597cdc9f4ab817d41e97
Szczepan Zalega <szczepan@nitrokey.com>
2023-04-01 18:50:06 +0200

    Update

--- config.fish
+++ config.fish
@@ -4,7 +4,9 @@ alias gb='git branch'
 alias gbb='git blame'
 alias gch='git checkout'
 alias gom='git checkout master'
-alias gc='git commit -S'
+alias gc='git commit'
+alias grem='git remote -v'
+alias gcm='git commit -m'
 alias gcma='git commit -S -asm'
 alias gcam='git commit -S -asm'
 alias gs='git status'
@@ -20,7 +22,13 @@ alias scan_network="sudo arp-scan -l --interface=enp0s25"
 alias pastebinit='pastebinit -b http://dpaste.com/'
 alias mv='mv -v'
 alias reload='source ~/.config/fish/config.fish'
+alias ghrev='gh pr list -S "user-review-requested:@me"'
+alias print_pipe='tee /dev/fd/2'
 
+# push out 3333 port for debugging
+alias drun='docker run -it --rm --privileged  -v $PWD:/app -p 3333:3333  (docker images | fzf | tee /dev/fd/2 | col3)'
+# X support
+alias drun_all='docker run -it --rm --privileged --network host -v $PWD:/app --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"  (docker images | fzf | tee /dev/fd/2 | col3)'
 
 # set -gx PATH ~/work/software/gcc-arm/gcc-arm-none-eabi-8-2018-q4-major/bin/ $PATH
 set -gx PATH /home/sz/work/software/gcc-arm-current-bin/ $PATH
@@ -29,13 +37,16 @@ set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH /usr/local/cuda/bin $PATH
 set -gx LD_LIBRARY_PATH /usr/local/cuda/lib64 $LD_LIBRARY_PATH
 
-# include this plugin so nix will work
-# https://github.com/NixOS/nix/issues/1512
-# https://github.com/oh-my-fish/plugin-foreign-env
-set fish_function_path $fish_function_path $HOME/.config/fish/plugin-foreign-env/functions
+	# include this plugin so nix will work
+	# https://github.com/NixOS/nix/issues/1512
+	# https://github.com/oh-my-fish/plugin-foreign-env
+	# set fish_function_path $fish_function_path $HOME/.config/fish/plugin-foreign-env/functions
 
-# initialize nix
-fenv source '$HOME/.nix-profile/etc/profile.d/nix.sh'
+	# initialize nix
+	# fenv source '$HOME/.nix-profile/etc/profile.d/nix.sh'
 
 export EDITOR=nvim
+export CARGO_TARGET_DIR=/tmp/cargo
+
 status --is-login; and status --is-interactive; and exec byobu-launcher
+
